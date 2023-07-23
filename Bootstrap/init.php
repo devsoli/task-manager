b6d28926dd4f1ea3b024068a701fa675c52f5244
Abolfazl Soltani <1abolfazl.soltani1@gmail.com>
2023-07-23 04:55:52 +0330

    create Connection to databse

--- Bootstrap/init.php
+++ Bootstrap/init.php
@@ -1,1 +1,15 @@
 <?php
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+include 'Bootstrap/config.php';
+try {
+    $conn = new PDO("mysql:host={$db_config->host};dbname=$db_config->name", $db_config->user, $db_config->pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Error Connection to Database : " . $e->getMessage();
+}
+
+include 'Libs/lib-tasks.php';
+include 'Libs/lib-folders.php';
