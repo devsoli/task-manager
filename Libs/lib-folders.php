60e2248c9c518343e1074d60e57355c4e8d7276a
Abolfazl Soltani <1abolfazl.soltani1@gmail.com>
2023-07-25 08:05:03 +0330

    create addFolder Function

--- Libs/lib-folders.php
+++ Libs/lib-folders.php
@@ -1,1 +1,9 @@
 <?php
+
+function addFolder($folderName) {
+    global $pdo;
+    $sql="INSERT INTO folders (name) VALUES (?)";
+    $pdo->prepare($sql)->execute([$folderName]);
+    return $pdo->lastInsertId();
+};
+
--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+include 'Bootstrap/init.php';
+
+if (isset($_POST['addFolder'])){
+    $folderName=$_POST['folderName'];
+    if (!empty($folderName)){
+        addFolder($folderName);
+        $message=(object)[
+            'type'=>'success',
+            'text'=>"Done, $folderName Add."
+        ];
+    }else{
+        $message=(object)[
+            'type'=>'danger',
+            'text'=>"Not Done, Please Enter Folder Name."
+        ];
+    }
+
+}
+
+include 'Tpl/tpl-index.php';
