865470fd77c8f2fba3235750a9d62c22842c5176
Abolfazl Soltani <1abolfazl.soltani1@gmail.com>
2023-07-25 09:04:02 +0330

    add getFolders Function

--- Libs/lib-folders.php
+++ Libs/lib-folders.php
@@ -7,3 +7,13 @@ function addFolder($folderName) {
     return $pdo->lastInsertId();
 };
 
+function getFolders(){
+    global $pdo;
+    $sql="SELECT * FROM folders";
+    $stmt=$pdo->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
+
+
+}
+
